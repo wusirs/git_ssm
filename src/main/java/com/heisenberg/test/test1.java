5214b3104d863e85ed6cac81c3c35684fc87ae87
wusirs <2411851860@qq.com>
2021-09-14 16:51:03 +0800

    测试

---
+++ src/main/java/com/heisenberg/test/test1.java
@@ -0,0 +1,12 @@
+package com.heisenberg.test;
+
+/**
+ * ClassName: test1 <br/>
+ * Description: <br/>
+ * date: 2021/9/14 16:50<br/>
+ *
+ * @author wulei<br />
+ * @since JDK 11
+ */
+public class test1 {
+}
