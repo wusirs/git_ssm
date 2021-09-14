a03b7b24c6159497099d89f33cea5b0b32873b96
wusirs <2411851860@qq.com>
2021-09-14 16:43:39 +0800

    Initial commit

---
+++ src/main/java/com/heisenberg/test/Test.java
@@ -0,0 +1,15 @@
+package com.heisenberg.test;
+
+/**
+ * ClassName: Test <br/>
+ * Description: <br/>
+ * date: 2021/9/14 16:22<br/>
+ *
+ * @author wulei<br />
+ * @since JDK 11
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
